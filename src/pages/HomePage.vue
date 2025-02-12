a062c800198ec8b2623d34f90ba17a6d74dd8f50
Anthony <anthony71.nguyen@gmail.com>
2025-02-12 16:35:56 +0100

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+    <main>
+        <h1>HomePage</h1>
+    </main>
+</template>
+<script setup lang="ts">
+
+
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,73 @@
+<template>
+    <main class="login__main">
+        <h1>LoginPage</h1>
+        <form v-on:submit.prevent="submitHandler">
+            <section>
+            <article>
+                <label for="email"></label>
+                <input 
+                v-model="data.email"
+                id="email"
+                type="email"
+                placeholder="entrez votre email"
+                class="input"
+                />
+            </article>
+            <article>
+                <label for="password"></label>
+                <input
+                v-model="data.password"
+                id="password"
+                type="password"
+                placeholder="entrez votre mot de passe"
+                class="input"
+                />
+            </article>
+        </section>
+        <section>
+            <button
+            type="submit"
+            class="button is-primary"
+            >Valider</button>
+            <button
+            type="reset"
+            class="button is-danger"
+            >réinitialiser</button>
+        </section>
+
+        </form>
+    </main>
+</template>
+<script lang="ts" setup>
+import {watch, reactive } from "vue"
+
+const data = reactive({
+    email: "",
+    password: ""
+})
+
+watch(data, (val) => {
+    console.log('Changement dans la variable data')
+})
+
+
+const isUserInputValid = (input:string): boolean => {
+    
+    const pattern = new RegExp()
+
+}
+
+const submitHandler = () => {
+
+}
+
+const inputHandler = function (){
+
+}
+
+</script>
+<style lang="scss" scoped>
+.login__main {
+
+}
+</style>
